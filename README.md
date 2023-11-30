6ca7ea663466d7decec9cc494c9c6e79aa8fa5ef
cl <li_cl@foxmail.com>
2023-12-01 01:52:21 +0800

    [new] Add loadtxt and format

--- README.md
+++ README.md
@@ -6,9 +6,11 @@
 ## 闲话
 可视化依赖于libigl, 可以去extern/libigl/tutorials查看用法。
 
-较大模型github无法上传, 请放在largeModels下面，不要污染了git仓库。
+为了保持代码风格与libigl一致，一律使用小写下划线命名变量、函数、文件夹名和文件名，用大驼峰命名类。
 
-ProjDirPath 是全局变量，获取到的是项目的根目录，它依赖于main.cpp的位置，因此不要随意移动main.cpp的位置。
+较大模型github无法上传, 请放在large_models下面，不要污染了git仓库。
+
+proj_dir_path 是全局变量，获取到的是项目的根目录，它依赖于main.cpp的位置，因此不要随意移动main.cpp的位置。
 
 增加新的cpp文件放在src下面，然后在CMakeLists.txt中添加即可。例如要增加another.cpp
 ```cmake
@@ -35,4 +37,10 @@ target_link_libraries(main PUBLIC
   # igl_restricted::mosek
   # igl_restricted::triangle
   )
+```
+
+
+下面这段代码中，pos必须是MatrixXd类型
+```
+viewer.data().set_mesh(pos, tri);
 ```
--- main.cpp
+++ main.cpp
@@ -1,69 +1,138 @@
-#include <igl/readOBJ.h>
+#include <igl/readOFF.h>
 #include <igl/opengl/glfw/Viewer.h>
 #include <iostream>
 #include <string>
+#include <fstream>
+#include <vector>
 
-std::string ProjDirPath;
+std::string proj_dir_path;
 
-Eigen::MatrixXd pos; //vertex positions
-Eigen::MatrixXi tri; //triangle indices
+Eigen::MatrixXd pos;  // vertex positions
+Eigen::MatrixXi tri;  // triangle indices
+Eigen::MatrixXi edge; // edge indices
+Eigen::MatrixXf rest_len;
 
-std::string GetDirectoryPath(const std::string& filePath) {
-    size_t found = filePath.find_last_of("/\\");
-    if (found != std::string::npos) {
-        return filePath.substr(0, found);
-    }
-    return "";
+std::string get_directory_path(const std::string &filePath)
+{
+  size_t found = filePath.find_last_of("/\\");
+  if (found != std::string::npos)
+  {
+    return filePath.substr(0, found);
+  }
+  return "";
 }
 
+void loadtxt(std::string filename, Eigen::MatrixXi &M)
+{
+  printf("Loading %s with integer\n", filename.c_str());
+
+  std::ifstream inputFile(filename);
+  std::string line;
+  std::vector<int> values;
+  unsigned int rows = 0;
+  unsigned int cols = 0;
+  while (std::getline(inputFile, line))
+  {
+    std::istringstream iss(line);
+    int val;
+    while (iss >> val)
+    {
+      values.push_back(val);
+      cols++;
+    }
+    rows++;
+  }
+  unsigned int numPerRow = static_cast<unsigned int>(cols / rows);
+  M.resize(rows, numPerRow);
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < numPerRow; j++)
+    {
+      M(i, j) = values[i * numPerRow + j];
+    }
+  }
+}
+
+void loadtxt(std::string filename, Eigen::MatrixXf &M)
+{
+  printf("Loading %s with float\n", filename.c_str());
+  std::ifstream inputFile(filename);
+  std::string line;
+  std::vector<float> values;
+  unsigned int rows = 0;
+  unsigned int cols = 0;
+  while (std::getline(inputFile, line))
+  {
+    std::istringstream iss(line);
+    int val;
+    while (iss >> val)
+    {
+      values.push_back(val);
+      cols++;
+    }
+    rows++;
+  }
+  unsigned int num_per_row = static_cast<unsigned int>(cols / rows);
+  M.resize(rows, num_per_row);
+  for (int i = 0; i < rows; i++)
+  {
+    for (int j = 0; j < num_per_row; j++)
+    {
+      M(i, j) = values[i * num_per_row + j];
+    }
+  }
+}
 
 int main(int argc, char *argv[])
 {
   // find the project directory path
-  std::string mainCppPath = __FILE__;
-  ProjDirPath = GetDirectoryPath(mainCppPath);
-  std::cout << "Project directory path: " << ProjDirPath << std::endl;
+  std::string main_path = __FILE__;
+  proj_dir_path = get_directory_path(main_path);
+  std::cout << "Project directory path: " << proj_dir_path << std::endl;
 
   // Load a mesh
-  igl::readOFF(ProjDirPath+"/data/models/bunny.OFF", pos, tri);
-  auto posOrig = pos; //keep original position for reset
+  igl::readOFF(proj_dir_path + "/data/models/bunny.OFF", pos, tri);
+  auto pos_orig = pos; // keep original position for reset
+
+  loadtxt(proj_dir_path + "/data/misc/edge.txt", edge);
+  loadtxt(proj_dir_path + "/data/misc/rest_len.txt", rest_len);
 
   // Visualization
   igl::opengl::glfw::Viewer viewer;
   viewer.data().set_mesh(pos, tri);
   // viewer.data().invert_normals = true;
 
-  viewer.callback_key_pressed = 
-    [&](igl::opengl::glfw::Viewer & viewer,unsigned int key,int mods)->bool
+  viewer.callback_key_pressed =
+      [&](igl::opengl::glfw::Viewer &viewer, unsigned int key, int mods) -> bool
   {
-    switch(key)
+    switch (key)
     {
-      default: 
-        return false;
-      case ' ':
-        viewer.core().is_animating = !viewer.core().is_animating;
-        return true;
-      case 'r':
-        pos = posOrig;
-        viewer.data().set_vertices(pos);
-        viewer.data().compute_normals();
-        return true;
+    default:
+      return false;
+    case ' ':
+      viewer.core().is_animating = !viewer.core().is_animating;
+      return true;
+    case 'r':
+      pos = pos_orig;
+      viewer.data().set_vertices(pos);
+      viewer.data().compute_normals();
+      return true;
     }
   };
 
-  viewer.callback_pre_draw = [&](igl::opengl::glfw::Viewer & viewer)->bool
+  viewer.callback_pre_draw = [&](igl::opengl::glfw::Viewer &viewer) -> bool
   {
     glEnable(GL_CULL_FACE);
-    if(viewer.core().is_animating)
+    if (viewer.core().is_animating)
     {
       // DoSubstep();
-      for(int v = 0;v<pos.rows();v++)
+      for (int v = 0; v < pos.rows(); v++)
       {
-        pos(v,1) -= 0.001;
+        pos(v, 1) -= 0.001;
         // collide with y=0 plane
-        if(pos(v,1) < 0)
+        if (pos(v, 1) < 0)
         {
-          pos(v,1) = 0.0;
+          pos(v, 1) = 0.0;
           // ~ coefficient of restitution
         }
       }

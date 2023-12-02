40e9bf058c70a08be7792894bc706459f48784a5
cl <li_cl@foxmail.com>
2023-12-02 20:28:05 +0800

    [new] using the thrust universal vector - using parallel_for

--- main.cu
+++ main.cu
@@ -3,22 +3,33 @@
 #include <fstream>
 #include <vector>
 #include <sstream>
+#include <cstdio>
+
+#include <cuda_runtime.h>
+#include <thrust/universal_vector.h>
+#include "helper_cuda.h"
+
 #include <Eigen/Dense>
 #include "igl/readOFF.h"
 #include "igl/writeOBJ.h"
-#include "cuda_runtime.h"
+
 
 using namespace std;
 
 std::string proj_dir_path;
 
+using Field1f = thrust::universal_vector<float>;
+using Field3f = thrust::universal_vector<float3>;
+using Field3i = thrust::universal_vector<int3>;
+
 unsigned num_particles = 0;
 
 //we have to use pos_vis because libigl uses Eigen::MatrixXd
-static float3 *pos_d = NULL; // vertex positions in device
-static vector<float3> pos_h; // vertex positions in host
+
 Eigen::MatrixXd pos_vis;     // vertex positions for visualization 
 Eigen::MatrixXi tri;     
+
+Field3f pos_uni;
 
 std::string get_proj_dir_path()
 {
@@ -34,15 +45,12 @@ std::string get_proj_dir_path()
     return proj_dir_path;
 }
 
-
-__global__ void field_add_one(float3 *pos_d, unsigned num_particles)
-{
-    unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < num_particles)
-    {
-        // pos_d[idx].x += 1.0f;
-        pos_d[idx].y += 1.0f;
-        // pos_d[idx].z += 1.0f;
+//learn from https://github.com/parallel101/course/blob/2d30da61b442008c003f69225e6feca20a4ca7df/08/06_thrust/01/main.cu
+template <class Func>
+__global__ void parallel_for(int n, Func func) {
+    for (int i = blockDim.x * blockIdx.x + threadIdx.x;
+         i < n; i += blockDim.x * gridDim.x) {
+        func(i);
     }
 }
 
@@ -65,41 +73,37 @@ int main(int argc, char *argv[])
 
     igl::writeOBJ(proj_dir_path + "/data/models/bunny.obj", pos_vis, tri);
 
-    // copy pos_vis to vector<float3> pos_h
-    pos_h.resize(num_particles);
+    // copy pos_vis to vector<float3> pos_uni
+    pos_uni.resize(num_particles);
     for (int i = 0; i < num_particles; i++)
     {
-        pos_h[i].x = pos_vis(i, 0);
-        pos_h[i].y = pos_vis(i, 1);
-        pos_h[i].z = pos_vis(i, 2);
+        pos_uni[i].x = pos_vis(i, 0);
+        pos_uni[i].y = pos_vis(i, 1);
+        pos_uni[i].z = pos_vis(i, 2);
     }
 
-    // allocate pos_d and copy pos_h to pos_d
-    cudaMalloc((void **)&pos_d, num_particles * sizeof(float3));
-    cudaMemcpy(pos_d, pos_h.data(), num_particles * sizeof(float3), cudaMemcpyHostToDevice);
-    
-    // add one to each vertex
-    field_add_one<<<(num_particles + 255) / 256, 256>>>(pos_d, num_particles);
 
-    // copy pos_d to host
-    cudaMemcpy(pos_h.data(), pos_d, num_particles * sizeof(float3), cudaMemcpyDeviceToHost);
+    // add one to each vertex
+    parallel_for<<<num_particles / 512, 128>>>(num_particles, [pos = pos_uni.data()] __device__ (int i) {
+        pos[i].y += 1.0;
+    });
+    checkCudaErrors(cudaDeviceSynchronize());
 
     // print 10 vertices
     printf("After adding one:\n");
     for (int i = 0; i < 10; i++)
     {
-        printf("pos_h[%d] = (%f, %f, %f)\n", i, pos_h[i].x, pos_h[i].y, pos_h[i].z);
+        printf("pos_uni[%d] = (%f, %f, %f)\n", i, pos_uni[i].x, pos_uni[i].y, pos_uni[i].z);
     }
 
-    // copy pos_h to pos_vis
+    // copy pos_uni to pos_vis
     for (int i = 0; i < num_particles; i++)
     {
-        pos_vis(i, 0) = pos_h[i].x;
-        pos_vis(i, 1) = pos_h[i].y;
-        pos_vis(i, 2) = pos_h[i].z;
+        pos_vis(i, 0) = pos_uni[i].x;
+        pos_vis(i, 1) = pos_uni[i].y;
+        pos_vis(i, 2) = pos_uni[i].z;
     }
 
     igl::writeOBJ(proj_dir_path + "/data/models/bunny2.obj", pos_vis, tri);
 
-    cudaFree(pos_d);
 }

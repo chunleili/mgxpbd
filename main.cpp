f50ee197781cdf9e4bf2ef0672a0dfaf1a11b8d7
cl <li_cl@foxmail.com>
2023-12-02 19:46:42 +0800

    [new] add helper_cuda helper_string - add include directory - cmakelist part - delete utils.h

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <fstream>
 #include <vector>
-#include "utils.h"
 
 using namespace Eigen;
 using namespace std;
--- src/utils.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-#include <iostream>
-#include <string>
-#include <fstream>
-#include <vector>
